017f48684090a899d6a814f203f50f2e049a19c2
azmuth94 <dannyaksenov@gmail.com>
2015-08-21 17:19:55 -0400

    ruby is a godsend compared to this

--- WWDCBadgesMethods/FISAppDelegate.m
+++ WWDCBadgesMethods/FISAppDelegate.m
@@ -15,10 +15,31 @@
     return YES;
 }
 
-/*
- 
- * Define your methods between application:didFinishLaunchingWithOptions and the @end marker
- 
- */
+- (NSString *)badgeForSpeaker:(NSString *)speaker {
+    
+    NSString *badge = [NSString stringWithFormat:@"Hello, my name is %@.", speaker];
+    return badge;
+}
+
+
+- (NSArray *)badgesForSpeakers:(NSArray *)speakers {
+    
+    NSMutableArray *mSpeakers = [[NSMutableArray alloc]init];
+    for (NSUInteger i = 0; i < [speakers count]; i++) {
+        NSString *badge = [NSString stringWithFormat:@"Hello, my name is %@.", speakers[i]];
+        mSpeakers[i] = badge;
+    }
+    return mSpeakers;
+}
+
+- (NSArray *)greetingsAndRoomAssignmentsForSpeakers:(NSArray *)speakers {
+    
+    NSMutableArray *mGreetingsAndRoomAssignments = [[NSMutableArray alloc]init];
+    for (NSUInteger i = 0; i < [speakers count]; i++) {
+        NSString *badge = [NSString stringWithFormat:@"Welcome, %@! You'll be in dressing room %lu.", speakers[i],(i+1)];
+        mGreetingsAndRoomAssignments[i] = badge;
+    }
+    return mGreetingsAndRoomAssignments;
+}
 
 @end
